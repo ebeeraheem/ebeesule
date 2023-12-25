13c6d76922ba3d20b205b2fe58fad66148308441
Ibrahim Suleiman <ebeeraheem@gmail.com>
2023-12-25 19:29:15 +0100

    Menu now closes upon clicking link

--- script.js
+++ script.js
@@ -3,15 +3,14 @@ const showMenu = document.querySelector("#open-menu");
 const hideMenu = document.querySelector("#close-menu");
 
 const menu = document.querySelector(".header-nav");
+const menuItems = document.querySelectorAll(".header-nav > *");
 
 function openMenu() {
   menu.classList.add("open");
 }
-function closeMenu(event) {
-  if (event.target.closest("#close-menu")) {
-    menu.classList.remove("open");
-  }
+function closeMenu() {
+  menu.classList.remove("open");
 }
 
 showMenu.addEventListener("click", openMenu);
-hideMenu.addEventListener("click", closeMenu);
+menu.addEventListener("click", closeMenu);
